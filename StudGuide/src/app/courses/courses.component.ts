bd4852f3d3f9bb32bda34c73bd82d6551446492b
Dinara <dkalijanovna@gmail.com>
2021-04-27 12:40:04 +0600

    added new data

--- StudGuide/src/app/courses/courses.component.ts
+++ StudGuide/src/app/courses/courses.component.ts
@@ -21,4 +21,20 @@ export class CoursesComponent implements OnInit {
       this.courses = data;
     })
   }
+
+  getRate(rate){
+    if(rate<2 && rate>0) return "assets/star1.png";
+    else if(rate<3) return "assets/star2.png"
+    else if(rate<4) return "assets/star3.png"
+    else if(rate<5) return "assets/star4.png"
+    else if(rate>=5) return "assets/star5.png"
+    return "assets/star0.png"
+  }
+
+  getLevel(level_id){
+    if(level_id==1) return "assets/lvlo1.png"
+    else if(level_id==2) return "assets/lvlo2.png"
+    return "assets/lvlo3.png"
+  }
+
 }
