bf26af47d54cb9812cb66c207fd9f4ac43b1b3b5
Dinara <dkalijanovna@gmail.com>
2021-04-29 12:31:27 +0600

    search btn

--- StudGuide/src/app/app.component.ts
+++ StudGuide/src/app/app.component.ts
@@ -1,19 +1,24 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
+import { News } from './models';
+import { NewsService } from './news.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   title = 'StudGuide';
   
   static logged:boolean;
   searchText = "";
+  news: News[]=[];
+  searching=false
+  constructor(private newsService: NewsService){}
 
- 
-  constructor(){}
-
+  ngOnInit(){
+    this.getNews();
+  }
 
   login(){
     return AppComponent.logged;
@@ -24,9 +29,18 @@ export class AppComponent{
     localStorage.removeItem('token');
   }
 
+  getNews(){
+    this.newsService.getNews().subscribe((data)=>{
+      this.news = data;
+    })
+  }
   search(){
-    alert(`Searching... ${this.searchText}`)
-    this.searchText = "";
+    // alert(`Searching... ${this.searchText}`)
+    const data = this.searchText;
+    this.news = this.news.filter(
+      news => news.title.toLowerCase().includes(data)
+    );
+    if(this.news.length) this.searching=true
   }
   
   
